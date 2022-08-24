edff2cf8045e04b5cf1ed625d44d2c3b221943af
Alan Harper <475+aussiegeek@users.noreply.github.com>
2022-08-24 20:15:31 +1000

    Setup Buildkite test analytics (#47)

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   projects: ["<rootDir>", "<rootDir>/packages/*"],
+  reporters: ["default", "buildkite-test-collector/jest/reporter"],
+  testLocationInResults: true,
 };
