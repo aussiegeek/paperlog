3750b3f1aec448b9c607ac83a6ad29e34816127b
Alan Harper <475+aussiegeek@users.noreply.github.com>
2022-09-14 19:48:18 +1000

    Extract duplicated test arbitaries (#77)

--- packages/paperlog/src/filter.test.ts
+++ packages/paperlog/src/filter.test.ts
@@ -2,85 +2,12 @@ import * as fc from "fast-check";
 import { ParserContact } from "./parse";
 import { array, assert } from "superstruct";
 import { filterPota, filterSota, filterWwff } from "./filter";
-
-const callsign = fc
-  .tuple(fc.constant("VK"), fc.integer({ min: 0, max: 9 }))
-  .map(([prefix]) => [prefix].join(""));
-
-const parserContact = fc.record<ParserContact>({
-  call: callsign,
-  stationCallsign: fc.constant("zxcv1234"),
-  qsoDate: fc.constant("20220101"),
-  timeOn: fc.constant("1234"),
-  band: fc.constant("20m"),
-  mode: fc.constant("CW"),
-  freq: fc.constant(7),
-  rstSent: fc.constant("59"),
-  rstRcvd: fc.constant("59"),
-});
-
-const upperAlpha = fc.constantFrom(
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
-);
-
-const sotaAssocArb = fc
-  .tuple(fc.constantFrom("VK", "ZL"), fc.integer({ min: 0, max: 9 }))
-  .map(([prefix, number]) => [prefix, number].join(""));
-const sotaRefArb = fc
-  .tuple(
-    sotaAssocArb,
-    fc.stringOf(upperAlpha, { minLength: 1, maxLength: 3 }),
-    fc.stringOf(
-      fc.constantFrom("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"),
-      { minLength: 3, maxLength: 3 }
-    )
-  )
-  .map(([assoc, region, summit]) => `${assoc}/${region}-${summit}`);
-
-const potaRefArb = fc
-  .tuple(
-    fc.constantFrom("VK", "ZL"),
-    fc.stringOf(
-      fc.constantFrom("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"),
-      { minLength: 4, maxLength: 4 }
-    )
-  )
-  .map(([prefix, code]) => `${prefix}-${code}`);
-
-const wwffRefArb = fc
-  .tuple(
-    fc.constantFrom("VK", "ZL"),
-    fc.stringOf(
-      fc.constantFrom("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"),
-      { minLength: 4, maxLength: 4 }
-    )
-  )
-  .map(([prefix, code]) => `${prefix}FF-${code}`);
+import {
+  parserContactArb,
+  potaRefArb,
+  sotaRefArb,
+  wwffRefArb,
+} from "./testArbitaries";
 
 describe("filter", () => {
   describe("filter sota", () => {
@@ -89,7 +16,7 @@ describe("filter", () => {
         fc.property(
           fc.array(
             fc
-              .tuple(parserContact, fc.constantFrom(undefined, ""))
+              .tuple(parserContactArb(), fc.constantFrom(undefined, ""))
               .map(([c, sotaRef]) => {
                 return { ...c, sotaRef };
               })
@@ -105,7 +32,7 @@ describe("filter", () => {
       fc.assert(
         fc.property(
           fc.array(
-            fc.tuple(parserContact, sotaRefArb).map(([c, mySotaRef]) => {
+            fc.tuple(parserContactArb(), sotaRefArb()).map(([c, mySotaRef]) => {
               return {
                 ...c,
                 mySotaRef,
@@ -124,7 +51,7 @@ describe("filter", () => {
       fc.assert(
         fc.property(
           fc.array(
-            fc.tuple(parserContact, sotaRefArb).map(([c, sotaRef]) => {
+            fc.tuple(parserContactArb(), sotaRefArb()).map(([c, sotaRef]) => {
               return {
                 ...c,
                 sotaRef,
@@ -146,7 +73,7 @@ describe("filter", () => {
         fc.property(
           fc.array(
             fc
-              .tuple(parserContact, fc.constantFrom(undefined, ""))
+              .tuple(parserContactArb(), fc.constantFrom(undefined, ""))
               .map(([c, potaRef]) => {
                 return { ...c, potaRef };
               })
@@ -162,7 +89,7 @@ describe("filter", () => {
       fc.assert(
         fc.property(
           fc.array(
-            fc.tuple(parserContact, potaRefArb).map(([c, myPotaRef]) => {
+            fc.tuple(parserContactArb(), potaRefArb()).map(([c, myPotaRef]) => {
               return {
                 ...c,
                 myPotaRef,
@@ -183,7 +110,7 @@ describe("filter", () => {
         fc.property(
           fc.array(
             fc
-              .tuple(parserContact, fc.constantFrom(undefined, ""))
+              .tuple(parserContactArb(), fc.constantFrom(undefined, ""))
               .map(([c, wwffRef]) => {
                 return { ...c, wwffRef };
               })
@@ -199,7 +126,7 @@ describe("filter", () => {
       fc.assert(
         fc.property(
           fc.array(
-            fc.tuple(parserContact, wwffRefArb).map(([c, myWwffRef]) => {
+            fc.tuple(parserContactArb(), wwffRefArb()).map(([c, myWwffRef]) => {
               return {
                 ...c,
                 myWwffRef,
