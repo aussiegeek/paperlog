0dff4a16557c5ed20e8e673e196a4b4362de859f
Alan Harper <475+aussiegeek@users.noreply.github.com>
2022-10-29 18:05:29 +1100

    Stop double running each test (#130)

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  projects: ["<rootDir>", "<rootDir>/packages/*"],
+  projects: ["<rootDir>/packages/*"],
   reporters: ["default", "buildkite-test-collector/jest/reporter"],
   testLocationInResults: true,
 };
