617e341243b9d3adbfd75298808c3a50e90dc9f9
Alan Harper <475+aussiegeek@users.noreply.github.com>
2026-06-06 15:58:19 +1000

    Remove paperlog-web package (#179)

--- packages/paperlog-web/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
-import "./index.css";
-
-const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
